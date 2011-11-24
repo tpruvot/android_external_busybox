f2e44a36fe982d227c3e1a04cd9f5b9b5e8cb4da
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2011-11-24 23:32:23 +0100

    vi: fix end key "lock" which require Ctrl+c

--- editors/vi.c
+++ editors/vi.c
@@ -3069,9 +3069,8 @@ static void do_cmd(int c)
 	int cnt, i, j;
 	int c1;
 
-//	c1 = c; // quiet the compiler
-//	cnt = yf = 0; // quiet the compiler
-//	p = q = save_dot = buf; // quiet the compiler
+	c1 = cnt = 0;
+	p = q = save_dot = NULL;
 	memset(buf, '\0', sizeof(buf));
 
 	show_status_line();
@@ -3317,12 +3316,19 @@ static void do_cmd(int c)
 #endif /* FEATURE_VI_YANKMARK */
 	case '$':			// $- goto end of line
 	case KEYCODE_END:		// Cursor Key End
+		if (--cmdcnt > 0) {
+			dot_next();
+			do_cmd(c);
+		}
+		dot = end_line(dot);
+/*
 		for (;;) {
 			dot = end_line(dot);
 			if (--cmdcnt > 0)
 				break;
 			dot_next();
 		}
+*/
 		break;
 	case '%':			// %- find matching char of pair () [] {}
 		for (q = dot; q < end && *q != '\n'; q++) {
